ac8c4106291e890f9af8d245c800eb6d9ea5be1b
Flibouche <kevin.pfiffer2@gmail.com>
2024-10-18 14:09:10 +0200

    Add all page and install framer-motion.

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 
 // Components
 import Header from "@/components/Header";
+import PageTransition from "@/components/PageTransition";
 
 const jetbrainsMono = JetBrains_Mono({
     subsets: ["latin"],
@@ -21,7 +22,9 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
         <html lang="en">
             <body className={jetbrainsMono.variable}>
                 <Header />
-                {children}
+                <PageTransition>
+                    {children}
+                </PageTransition>
             </body>
         </html>
     );
