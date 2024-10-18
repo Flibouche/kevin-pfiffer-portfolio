232e325a3322183ab040c8aa3018c002f8c4293d
Flibouche <kevin.pfiffer2@gmail.com>
2024-10-18 13:57:28 +0200

    Create desktop header links.

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,32 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+
+// Components
+import Nav from "./Nav";
+
 const Header = () => {
     return (
-        <header>header</header>
+        <header className="py-8 xl:py-12 text-white">
+            <div className="container mx-auto flex justify-between items-center">
+                {/* Logo */}
+                <Link href="/">
+                    <h1 className="text-4xl font-semibold">
+                        Kevin <span className="text-accent">.</span>
+                    </h1>
+                </Link>
+
+                {/* Desktop nav & Hire me button */}
+                <div className="hidden xl:flex items-center gap-8">
+                    <Nav />
+                    <Link href="/contact">
+                        <Button>Hire me</Button>
+                    </Link>
+                </div>
+
+                {/* Mobile nav */}
+                <div className="xl:hidden">mobile nav</div>
+            </div>
+        </header>
     )
 }
 
