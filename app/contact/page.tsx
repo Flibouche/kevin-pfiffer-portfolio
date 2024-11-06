51b9d5e22525a5959910c8f114563917d6ffd5ff
Flibouche <kevin.pfiffer2@gmail.com>
2024-11-06 11:02:06 +0100

    👌 IMPROVE: add correction to the email informations, add a message of success or failure and a timeout.

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useRef } from "react";
+import { useState } from "react";
 import { motion } from "framer-motion";
 import emailjs from 'emailjs-com';
 
@@ -31,23 +31,50 @@ const info = [
 ];
 
 const Contact = () => {
-    const form = useRef<HTMLFormElement | null>(null);
+    const [form, setForm] = useState({
+        name: '',
+        email: '',
+        message: ''
+    });
+    const [message, setMessage] = useState('');
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        const { name, value } = e.target;
+        setForm((prevForm) => ({
+            ...prevForm,
+            [name]: value,
+        }));
+    };
 
     const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
 
-        if (form.current) {
-            emailjs
-                .sendForm('service_y3rr1zg', 'template_bxvz90w', form.current, 'ScuE2nS3m1Twxa2LR')
-                .then(
-                    () => {
-                        console.log('SUCCESS!');
-                    },
-                    (error) => {
-                        console.log('FAILED...', error.text);
-                    },
-                );
-        }
+        emailjs
+            .send(
+                'service_y3rr1zg',
+                'template_bxvz90w',
+                {
+                    from_name: form.name,
+                    to_name: "Kevin Pfiffer",
+                    from_email: form.email,
+                    to_email: "kevin.pfiffer2@gmail.com",
+                    message: form.message
+                },
+                'ScuE2nS3m1Twxa2LR'
+            )
+            .then(
+                () => {
+                    console.log('SUCCESS!');
+                    setMessage('Votre message a bien été envoyé !');
+                    setTimeout(() => setMessage(''), 5000);
+                    setForm({ name: '', email: '', message: '' });
+                },
+                (error) => {
+                    setMessage('Une erreur est survenue, veuillez réessayer plus tard.');
+                    setTimeout(() => setMessage(''), 5000);
+                    console.log('FAILED...', error.text);
+                },
+            );
     };
 
     return (
@@ -59,25 +86,41 @@ const Contact = () => {
             }}
             className='py-6'
         >
+            {message && <p className="bg-accent my-4 text-center text-white">{message}</p>}
             <div className="container mx-auto">
                 <div className="flex flex-col xl:flex-row gap-[30px]">
                     {/* Form */}
                     <div className="xl:w-[54%] order-2 xl:order-none">
-                        <form ref={form} onSubmit={sendEmail} className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
+                        <form onSubmit={sendEmail} className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
                             <h3 className="text-4xl text-accent">Envoyez-moi un message !</h3>
                             <p className="text-white/60">Je suis ouvert aux propositions d'alternance pour le titre de Concepteur Développeur d'Applications ou aux propositions d'emplois.</p>
                             {/* Input */}
                             <div className="flex flex-col gap-6">
-                                <Input type="name" placeholder="Quel est votre nom ?" />
-                                <Input type="email" placeholder="Quelle est votre adresse email ?" />
+                                <Input
+                                    type="text"
+                                    name="name"
+                                    value={form.name}
+                                    onChange={handleChange}
+                                    placeholder="Quel est votre nom ?"
+                                />
+                                <Input
+                                    type="email"
+                                    name="email"
+                                    value={form.email}
+                                    onChange={handleChange}
+                                    placeholder="Quelle est votre adresse email ?"
+                                />
                             </div>
                             {/* Textarea */}
                             <Textarea
+                                name="message"
+                                value={form.message}
+                                onChange={handleChange}
                                 className="h-[200px]"
                                 placeholder="Que voulez-vous me dire ?"
                             />
                             {/* Buttons */}
-                            <Button size="md" className="max-w-40" type="submit" value="Send">Envoyer</Button>
+                            <Button size="md" className="max-w-40" type="submit">Envoyer</Button>
                         </form>
                     </div>
                     {/* Info */}
@@ -101,7 +144,7 @@ const Contact = () => {
                 </div>
             </div>
         </motion.section >
-    )
+    );
 }
 
-export default Contact
+export default Contact;
