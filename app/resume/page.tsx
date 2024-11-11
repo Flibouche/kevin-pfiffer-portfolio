2d3ad47786b887727ffcec148b9108de29d326bf
Flibouche <kevin.pfiffer2@gmail.com>
2024-11-11 23:53:27 +0100

    🐛 FIX: reduce the size of the about me informations.

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -703,7 +703,7 @@ const Resume = () => {
                                         return (
                                             <li key={index} className='flex items-center justify-center xl:justify-start gap-4'>
                                                 <span className='text-white/60 text-base'>{item.fieldName}</span>
-                                                <span className='text-lg'>{item.fieldValue}</span>
+                                                <span className='text-md'>{item.fieldValue}</span>
                                             </li>
                                         )
                                     })}
